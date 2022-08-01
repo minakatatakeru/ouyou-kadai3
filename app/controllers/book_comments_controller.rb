06ec64b3937ca025a45ce680721bab86537cb622
minakatatakeru <ec2-user@ip-172-31-43-59.ec2.internal>
2022-08-01 07:07:38 +0000

    Finish ouyou-kadai6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,13 +5,14 @@ class BookCommentsController < ApplicationController
         @book_comment.book_id = @book.id
         @book_comment.user_id = current_user.id
         @book_comment.save
+        @book_comment = BookComment.new
     end
     
     def destroy
       @book = Book.find(params[:book_id])
       @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
       @book_comment.destroy
-      @book_comment = BookComment.new(book_comment_params)
+      @book_comment = BookComment.new
     end
     
     private
